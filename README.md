f821eb6a081cf8c96c34c4e43e5b03e7c70677bd
Don <donkeeting@gmail.com>
2017-03-15 11:01:38 -0500

    Updating the library and readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # starwars-names
+
+usage:
+var names = require('starwars-names');
+var allNames = names.all;
+var randomName = names.random();
+var threeRandomNames = names.random(3);
+
+This library was developed by Kent C. Dodds as part of an egghead.io series called "How to Write a JavaScript Library."
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
 
+const getRandomItem = uniqueRandomArray(starWarsNames);
+
 module.exports = {
-	all: starWarsNames,
-	random: uniqueRandomArray(starWarsNames)
+  all: starWarsNames,
+  random: random
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    const randomItems = [];
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
